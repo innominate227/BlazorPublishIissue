7359854c6cc7f3ca703c01e2f8db83f8f38b6358
Nate Barish <nate.barish@magnitudecapital.com>
2021-09-24 17:04:57 -0400

    init commit

---
+++ ClassLibrary4dot8/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NLog;
+
+namespace ClassLibrary4dot8
+{
+    public class Class1
+    {
+
+        public string Method()
+        {
+            var logInfo = new LogEventInfo(LogLevel.Info, "Test", "Test From Class Library using dotnet 4.8");
+            return logInfo.Message;
+        }
+
+    }
+}
---
+++ ClassLibrary5/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+
+namespace ClassLibrary5
+{
+    public class Class1
+    {
+
+        public string Method()
+        {
+            var logInfo = new LogEventInfo(LogLevel.Info, "Test", "Test From Class Library using dotnet 5.0");
+            return logInfo.Message;
+        }
+    }
+}
